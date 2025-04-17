5254f30b27bb8352840d6d1694fc5bc0e644f494
hank <hank29206880@gmail.com>
2025-04-17 15:17:44 +0900

    week7

--- Week-7/4.1.prim.py
+++ Week-7/4.1.prim.py
@@ -5,11 +5,30 @@ def prim(n: int, W: List[List[float]]) -> List[Tuple[int, int, float]]:
     nearest = [-1] * (n + 1)
     distance = [-1] * (n + 1)
     INF = float("inf")
+
     for i in range(2, n + 1):
         nearest[i] = 1
         distance[i] = W[1][i]
+
     for _ in range(n - 1):
-        min = INF
-        # Complete the code here
+        min_dist = INF
+        vnear = -1
+        for i in range(2, n + 1):
+            if 0 <= distance[i] < min_dist:
+                min_dist = distance[i]
+                vnear = i
+
+        if vnear == -1:
+             break
+
+        edge = (nearest[vnear], vnear, W[nearest[vnear]][vnear])
+        F.append(edge)
+
+        distance[vnear] = -1
+
+        for i in range(2, n + 1):
+            if W[i][vnear] < distance[i]:
+                distance[i] = W[i][vnear]
+                nearest[i] = vnear
 
     return F
--- Week-7/4.3.dijkstra.py
+++ Week-7/4.3.dijkstra.py
@@ -5,11 +5,29 @@ def dijkstra(n: int, W: List[List[float]]) -> List[Tuple[int, int, float]]:
     INF = float("inf")
     touch = [-1] * (n + 1)
     length = [-1] * (n + 1)
+
     for i in range(2, n + 1):
         touch[i] = 1
         length[i] = W[1][i]
-    for _ in range(n - 1):
-        min = INF
-        # Complete the code here
 
+    for _ in range(n - 1):
+        min_len = INF
+        vnear = -1
+        for i in range(2, n + 1):
+            if 0 <= length[i] < min_len:
+                min_len = length[i]
+                vnear = i
+
+        if vnear == -1:
+            break
+
+        edge = (touch[vnear], vnear, W[touch[vnear]][vnear])
+        F.append(edge)
+
+        for i in range(2, n + 1):
+            if W[vnear][i] != INF and length[vnear] + W[vnear][i] < length[i]:
+                length[i] = length[vnear] + W[vnear][i]
+                touch[i] = vnear
+
+        length[vnear] = -1
     return F
