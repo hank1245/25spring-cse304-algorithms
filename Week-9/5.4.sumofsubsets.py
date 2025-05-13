2f2f2e8a0e8aa0816b39f5575e5b487572423ddc
hank <hank29206880@gmail.com>
2025-05-13 15:24:39 +0900

    week-9:backtracking

--- Week-9/5.4.sumofsubsets.py
+++ Week-9/5.4.sumofsubsets.py
@@ -7,12 +7,16 @@ include: List[int]
 
 def promising(i: int, weight: int, total: int) -> bool:
     global n, W, w, include
-    # Complete the code here
+    return (weight + total >= W) and (weight == W or weight + w[i+1] <= W)
 
 def sumofsubsets(i: int, weight: int, total: int) -> None:
     global n, W, w, include
     if promising(i, weight, total):
         if weight == W:
-            print("found:", include[1:])
+            print(f"found:{include[1:]}")
         else:
-            # Complete the code here
+            include[i+1] = 1
+            sumofsubsets(i+1, weight + w[i+1], total - w[i+1])
+            
+            include[i+1] = 0
+            sumofsubsets(i+1, weight, total - w[i+1])
