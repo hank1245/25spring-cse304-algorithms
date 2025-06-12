c72cf4ddf58e5cfab536babb1a7a6e5589292190
hank <hank29206880@gmail.com>
2025-06-12 15:26:13 +0900

    week13

--- Week-13/7.5.heapsort.py
+++ Week-13/7.5.heapsort.py
@@ -2,19 +2,44 @@ from typing import List
 
 class Heap:
     def __init__(self, n: int, S: List[int]) -> None:
-        # Complete the code here
+        self.size = n
+        self.H = S
 
 def siftdown(H: Heap, i: int) -> None:
-    # Complete the code here
+    siftkey = H.H[i]
+    parent = i
+    spotfound = False
+    
+    while 2 * parent <= H.size and not spotfound:
+        if 2 * parent < H.size and H.H[2 * parent] < H.H[2 * parent + 1]:
+            largerchild = 2 * parent + 1
+        else:
+            largerchild = 2 * parent
+        
+        if siftkey < H.H[largerchild]:
+            H.H[parent] = H.H[largerchild]
+            parent = largerchild
+        else:
+            spotfound = True
+    
+    H.H[parent] = siftkey
 
 def root(H: Heap) -> int:
-    # Complete the code here
+    keyout = H.H[1]
+    H.H[1] = H.H[H.size]
+    H.size -= 1
+    if H.size > 0:
+        siftdown(H, 1)
+    return keyout
 
 def removekeys(n: int, H: Heap, S: List[int]) -> None:
-    # Complete the code here
+    for i in range(n, 0, -1):
+        S[i] = root(H)
 
 def makeheap(n: int, H: Heap) -> None:
-    # Complete the code here
+    for i in range(n // 2, 0, -1):
+        siftdown(H, i)
 
 def heapsort(n: int, H: Heap, S: List[int]) -> None:
-    # Complete the code here
+    makeheap(n, H)
+    removekeys(n, H, S)
